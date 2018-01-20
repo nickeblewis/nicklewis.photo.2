0dd3f13c6f3dee50ca59bbec3a6965fedfa6b4c6
Ryan Wiemer <ryan@ryanwiemer.com>
2018-01-19 20:00:09 -0800

    Force browsers to start at the top of the page on route changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,11 @@ import objectFitImages from 'object-fit-images'
 exports.onInitialClientRender = () => {
   objectFitImages()
 }
+
+exports.onRouteUpdate = () => {
+  if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
+}
+
+exports.shouldUpdateScroll = args => {
+   return false;
+};
