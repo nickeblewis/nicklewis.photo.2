dac7723bff67218a29b386ebf17e05d557df258e
Ryan Wiemer <ryan@ryanwiemer.com>
2018-01-27 19:35:46 -0800

    Scrolling position improvements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,10 +4,17 @@ exports.onInitialClientRender = () => {
   objectFitImages()
 }
 
-exports.onRouteUpdate = () => {
-  if (typeof window !== `undefined`) { window.scrollTo(0, 0)}
+// Scrolling improvements as suggested by @dsbrianwebster
+// See https://github.com/ryanwiemer/rw/issues/1
+exports.onRouteUpdate = args => {
+  if (typeof window !== 'undefined' && args.action == "PUSH") {
+    window.scrollTo(0, 0)
+  }
 }
 
 exports.shouldUpdateScroll = args => {
-   return false;
+  return (
+    args.prevRouterProps == null ||
+    (args.prevRouterProps && args.prevRouterProps.history.action == "POP")
+  ) ? true : false;
 };
